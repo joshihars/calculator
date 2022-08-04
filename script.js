37109d563f3f636a4407a6bc8a9c716217f9d132
Harshit Joshi <93377385+joshihars@users.noreply.github.com>
2022-08-04 13:15:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,92 +1,128 @@
+// function for getting result 
 function estimate(){
-    let expressionElements = output.value.split("");
-    console.log(expressionElements);
+
+    // using split for spliting the expression
+
+    let estimateElements = output.value.split(""); 
+    console.log(estimateElements);
+
+    // loop for fetching the expression element one by one
+
+
     let result;
-    for (let i = 0; i < expressionElements.length; i++) {
-        const element = expressionElements[i];
-        if (element == "(" && i > 0) {
+    for (let i = 0; i < estimateElements.length; i++) {
+        const element = estimateElements[i];
+
+
+        // using if else for checking the value before (
+
+
+        if (element == "(" && i > 0) {   
             if (
-              expressionElements[i-1] == "+" || 
-              expressionElements[i-1] == "-" ||
-              expressionElements[i-1] == "*" ||
-              expressionElements[i-1] == "/" ||
-              expressionElements[i-1] == "(" || 
-              expressionElements[i-1] == "*(" ||
-              expressionElements[i-3] == "s" || 
-              expressionElements[i-3] == "t" ||
-              expressionElements[i-3] == "c" || 
-              expressionElements[i-3] == "*s" ||
-              expressionElements[i-3] == "*c" || 
-              expressionElements[i-3] == "*t"
+              estimateElements[i-1] == "+" || 
+              estimateElements[i-1] == "-" ||
+              estimateElements[i-1] == "*" ||
+              estimateElements[i-1] == "/" ||
+              estimateElements[i-1] == "(" || 
+              estimateElements[i-1] == "*(" ||
+              estimateElements[i-3] == "s" || 
+              estimateElements[i-3] == "t" ||
+              estimateElements[i-3] == "c" || 
+              estimateElements[i-3] == "*s" ||
+              estimateElements[i-3] == "*c" || 
+              estimateElements[i-3] == "*t"
               ){
                 continue;            
             }
             else{
-                expressionElements[i] = "*(";
+                estimateElements[i] = "*(";
             }
         }
-        if (element == ")" && i < expressionElements.length - 1) {
+
+        //using if else for checking the value after )
+
+        if (element == ")" && i < estimateElements.length - 1) {
             if (
-               expressionElements[i+1]== "+" || 
-               expressionElements[i+1] == "-" || 
-               expressionElements[i+1] == "*" ||
-               expressionElements[i+1] == "/" || 
-               expressionElements[i+1] == ")"
+                estimateElements[i+1]== "+" || 
+                estimateElements[i+1] == "-" || 
+                estimateElements[i+1] == "*" ||
+                estimateElements[i+1] == "/" || 
+                estimateElements[i+1] == ")" ||
+                estimateElements[i+1] == "("
                ){
                 continue;
             }
             else{
-                expressionElements[i] = ")*";
+                estimateElements[i] = ")*";
             }
         }
-    
+    // using if else for checking the value before sin,cos,tan
 
     if ((element == "s" ||  element == "c" || element == "t") && i > 0) {
         if (
-           expressionElements[i-1] == "+"||
-           expressionElements[i-1] == "-" ||
-           expressionElements[i-1] == "*" ||
-           expressionElements[i-1] == "/" ||
-           expressionElements[i-1] == "(" ||
-           expressionElements[i-1] == "*("
+            estimateElements[i-1] == "+"||
+            estimateElements[i-1] == "-" ||
+            estimateElements[i-1] == "*" ||
+            estimateElements[i-1] == "/" ||
+            estimateElements[i-1] == "(" ||
+            estimateElements[i-1] == "*("
            ){
             continue;
         }
         else{
-            if (element == "s" && expressionElements[i-1] != "o" && expressionElements[i-2] != "r")  {
-                expressionElements[i] = "*s";
+            if (element == "s" && estimateElements[i-1] != "o" && estimateElements[i-2] != "r")  {
+                estimateElements[i] = "*s";
             }
             else if (element == "c"){
-                expressionElements[i] = "*c";
+                estimateElements[i] = "*c";
             }
             else if (element == "t"){
-                expressionElements[i] = "*t";
+                estimateElements[i] = "*t";
             }
         }
     }
     
 }
-         console.log(expressionElements);
+         console.log(estimateElements);
+
+         //using if else for degree and radian conversion
+        
+        // for degree conversion 
 
     if (degreeBox.checked) {
-         answer = expressionElements.join("")
-         .replace(/sin\(/g, "Math.sin((Math.PI/180)*")
+
+        // join uesd for joining expression
+
+         answer = estimateElements.join("") 
+
+         // using regex for identifying and replacing with method 
+
+         .replace(/sin\(/g, "Math.sin((Math.PI/180)*") 
          .replace(/cos\(/g, "Math.cos((Math.PI/180)*")
          .replace(/tan\(/g, "Math.tan((Math.PI/180)*")
          .replace(/π/g, "Math.PI")
          .replace(/reMath.sin\(\(Math.PI\/180\)\*/g, "resin(");
     }
+
+       // for radian conversion 
+
     else{
-         answer = expressionElements.join("")
+
+        // join uesd for joining expression
+
+         answer = estimateElements.join("")
+
+         // using regex for identifying and replacing with method
+
          .replace(/sin/g, "Math.sin")
          .replace(/cos/g, "Math.cos")
          .replace(/tan/g, "Math.tan")
          .replace(/π/g, "3.14159")
          .replace(/reMath.sin/g, "resin");
-    }
+    } 
+ 
+     // using try catch for error handling 
 
-   
-    //console.log(some);
     try{
         result = eval(answer)
         output.value = result.toFixed(5); 
